5484e07c438347d3670f5a019f5cd882c998fb6b
Titouhi Omar <titouhiomar223@gmail.com>
2026-04-20 19:28:24 +0100

    ajout de la classe FileHelper et des éléments d'interface dans MainActivity

---
+++ app/src/main/java/com/omartitouhi/todolist/FileHelper.kt
@@ -0,0 +1,11 @@
+package com.omartitouhi.todolist
+
+import android.content.Context
+
+class FileHelper {
+
+    val FILENAME = "listeinfo.dat"
+
+    fun writeData(items: ArrayList<String>,context: Context){
+    }
+}
--- app/src/main/java/com/omartitouhi/todolist/MainActivity.kt
+++ app/src/main/java/com/omartitouhi/todolist/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.omartitouhi.todolist
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var item : EditText
+    lateinit var ajouter : Button
+    lateinit var list : ListView
+
+    var itemList = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +26,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        item = findViewById(R.id.note)
+        ajouter = findViewById(R.id.buttonadd)
+        list = findViewById(R.id.itemliste)
+
+
     }
 }
